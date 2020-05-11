2b90848178dea4480c5591e958bded16b1ea5dad
ahayat16 <61722512+ahayat16@users.noreply.github.com>
2020-05-11 17:37:15 -0400

    Update numerics_goatin.m

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -1,12 +1,17 @@
 nt=10000;
 nx=10000;
+nv=10000;
 dt=0.01;
 dx=0.01;
 
 t=dt.*(1:1:nt);
 x=dx.*(-nx-0.5:1:nx+0.5);
-v=dv.*(-nx-0.5:1:nx+0.5);
+v=dv.*(-nv-0.5:1:nv+0.5);
 
 lx=dt./dx;
 lv=dt./dv;
 hallo
+
+f=zeros(length(t),length(x),length(v));
+g=zeros(length(t),length(x),length(v));
+
