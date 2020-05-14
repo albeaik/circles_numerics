0a2c366283dc1f4d9bfcc4c2c4bc9435646e1064
BanachSteinhaus <65196421+BanachSteinhaus@users.noreply.github.com>
2020-05-13 18:58:34 -0700

    Added some lines, except for the nonlocal term everything should be in.

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -1,17 +1,23 @@
-nt=10000;
-nx=10000;
-nv=10000;
-dt=0.01;
-dx=0.01;
+nt=1e2;
+nx=1e3;
+nv=1e3;
+x=linspace(-10,10,nx);
+v=linspace(0,20,nv);
+t=linspace(0,10,nt);
 
-t=dt.*(1:1:nt);
-x=dx.*(-nx-0.5:1:nx+0.5);
-v=dv.*(-nv-0.5:1:nv+0.5);
+Q=zeros(nt+1,nx,nv);
+lambdax=(t(2)-t(1))/(x(2)-x(1));
+lambdav=(t(2)-t(1))/(v(2)-v(1));
+Q(1,:,:)=1/2*ones(nx,nv);
+V=@(x) x;   %needs to be changed to the actual functions
+h=@(x) x;
+theta=@(x,v) h(-x).*(V(x)-v).*(x<=epsilon).*(x>=0);
+epsilonx=1; %viscosity parameter, needs to be changed later to a proper value
+epsilonv=1;
 
-lx=dt./dx;
-lv=dt./dv;
-hallo
-
-f=zeros(length(t),length(x),length(v));
-g=zeros(length(t),length(x),length(v));
-
+for n=1:1:nt
+            Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
+            n
+            W=?????
+            Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,3:nv)) +epsilonv.*(-Q(n+1,2:nx-1,1:nv-2)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
+end
