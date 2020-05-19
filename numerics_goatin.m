f9346e02f1f8ed9059cb835a44ec672439ffefdc
xgong14 <44510249+xgong14@users.noreply.github.com>
2020-05-19 13:05:51 -0400

    Update numerics_goatin.m

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -22,23 +22,31 @@ Theta=theta(X1,V);
 W = zeros(nx,nv);
 
 for n=1:1:nt
-            Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
-            lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
-            epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
-            n
+                Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
+                lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
+                epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
+     
+            %Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
+            %lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
+            %epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
+            %n
             
             for i=1:1:nx
                         for j=1:1:nv
                                     s = zeros(1,nv);
                                     for l = -(nx-1):(nx-1)
 %                                                s=s+Theta(nx+i-l,j).*Q(n+1,l+nx,2:nv-1);
-                                                 s=s+Theta(i-l,j).*Q(n+1,l,2:nv-1);
+                                                 s=s+Theta(i-l,j).*Q(n+1,l,2:nv-1); 
                                    end
                                     W(i,j) = sum(s);
                         end
             end
             W = (x(2)-x(1)).*(v(2)-v(1)).*W;
-            Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
-            lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
-            epsilonv.*(-Q(n+1,2:nx-1,1:nv-2)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
+             Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
+             lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
+             epsilonv.*(-Q(n+1,2:nx-1,3:nv-1)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
+            
+           % Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
+           % lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
+           % epsilonv.*(-Q(n+1,2:nx-1,1:nv-2)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
 end
