d324092c7a30e843bc85acb031dbcf9424100512
BanachSteinhaus <65196421+BanachSteinhaus@users.noreply.github.com>
2020-05-19 19:17:21 -0700

    added plotting and more
    added in 52 a reshape command as the matrix array multiplication might give otherwise the wrong dimensions.
    Also removed the inner parloop as matlab cannot parloop on several levels.
    Code does work, but the boundary datum which seems to be wrong produces problems so that we are after a few iterations at NAN.

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -1,7 +1,9 @@
+clear all
 nt=1e2;
-nx=1e3;
-nv=1e3;
+nx=1e2;
+nv=1e2;
 Vmax =20; % needed when define function Vf
+epsilon=1;
 x=linspace(-10,10,nx);
 v=linspace(0,Vmax,nv);
 t=linspace(0,10,nt);
@@ -10,10 +12,14 @@ Q=zeros(nt+1,nx,nv);
 lambdax=(t(2)-t(1))/(x(2)-x(1));
 lambdav=(t(2)-t(1))/(v(2)-v(1));
 Q(1,:,:)=1/2*ones(nx,nv);
+Q(1,1,:)=0;
+Q(1,end,:)=0;
+Q(1,:,1)=0;
+Q(1,:,end)=0;
 d0 = 2.5; % needed when define function Vf. Change later. 
 Vf=@(x) Vmax.*((tanh(x./d0-2)+tanh(2))./(1+tanh(2)));  
-h=@(x) exp(-(1)./((epsilonx./2).^2-(-x-epsilonx/2).^2)).*(x>-epsilonx).*(x<0);
-theta=@(x,v) h(-x).*(Vf(x)-v).*(x<=epsilonx).*(x>=0);
+h=@(x) exp(-(1)./((epsilon./2).^2-(-x-epsilon/2).^2)).*(x>-epsilon).*(x<0);
+theta=@(x,v) h(-x).*(Vf(x)-v).*(x<=epsilon).*(x>=0);
 epsilonx=1; %viscosity parameter, needs to be changed later to a proper value
 epsilonv=1;
 x1 = linspace(-20,20,2.*nx);
@@ -25,14 +31,14 @@ for n=1:1:nt
                 Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
                 lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
                 epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
-     
+     n
             %Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
             %lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
             %epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
             %n
             
             parfor i=1:1:nx
-                        parfor j=1:1:nv
+                        for j=1:1:nv
                                     s = zeros(1,nv);
                                     for l = 1:nx
 %                                                s=s+Theta(nx+i-l,j).*Q(n+1,l+nx,2:nv-1);
@@ -43,9 +49,13 @@ for n=1:1:nt
             end
             W = (x(2)-x(1)).*(v(2)-v(1)).*W;
              Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
-             lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
-             epsilonv.*(-Q(n+1,2:nx-1,3:nv-1)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
+             lambdav.*reshape(W(2:nx-1,2:nv-1).*(reshape(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2),nx-2,nv-2)),1,nx-2,nv-2) +...
+             epsilonv.*(-Q(n+1,2:nx-1,3:nv)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
             
+         figure(1)
+         surf(reshape(Q(n,:,:),nx,nv));
+         zlim([0,1]);
+         drawnow;
            % Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
            % lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
            % epsilonv.*(-Q(n+1,2:nx-1,1:nv-2)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
