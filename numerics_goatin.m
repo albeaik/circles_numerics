50c6d6b870ec5d57eee9a82dc0d415b501ee32a4
BanachSteinhaus <65196421+BanachSteinhaus@users.noreply.github.com>
2020-05-23 22:47:52 -0700

    Removed another loop for the computation of W
    Had some side effects, as parloop does not work then anymore, also the new computation of W does not give the same as the old but I would guess the old was wrong.

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -1,16 +1,16 @@
-
 clear all
-nt=1e2;
-nx=1e2+1;
-nv=1e2;
-Vmax =20; % needed when define function Vf
+close all
+nt=5e3;
+nx=2e2+1;
+nv=2e2;
+Vmax =10; % needed when define function Vf
 epsilon=1;
-x=linspace(-10,10,nx);
-v=linspace(0,Vmax,nv);
-t=linspace(0,1,nt);
+x=linspace(-0,20,nx);
+v=linspace(0,20,nv);
+t=linspace(0,10,nt);
 
 Q=zeros(nt+1,nx,nv);
-Q0=@(x,v) (x>=2).*(x<=5).*(v>=10).*(v<=15);
+Q0=@(x,v) (x>=2).*(x<=5).*(v>=10).*(v<=15) + (x>=5).*(x<=15).*(v>=5).*(v<=10);
 
 
 lambdax=(t(2)-t(1))/(x(2)-x(1));
@@ -25,24 +25,37 @@ d0 = 2.5; % needed when define function Vf. Change later.
 Vf=@(x) Vmax.*((tanh(x./d0-2)+tanh(2))./(1+tanh(2)));  
 h=@(x) exp(-(1)./((epsilon./2).^2-(-x-epsilon/2).^2)).*(x>-epsilon).*(x<0);
 theta=@(x,v) h(-x).*(Vf(x)-v).*(x<=epsilon).*(x>=0);
-epsilonx=1; %viscosity parameter, needs to be changed later to a proper value
-epsilonv=1;
+epsilonx=1/3; %viscosity parameter, needs to be changed later to a proper value
+epsilonv=1/3;
+if lambdax > 2*min(3*epsilonx,2-3*epsilonx)/7
+    disp('CFL condition in x not satisfied')
+elseif epsilonx>2/3 || epsilonx <0
+    disp('Viscosity approximation out of range')
+end
 x1 = linspace(-20,20,2.*nx);
 [V,X1]=meshgrid(v,x1);
 Theta=theta(X1,V);
+Thetaflip=flip(Theta);
 W = zeros(nx,nv);
 [V2,~]=meshgrid(v,ones(1,nx));
 [V,X]=meshgrid(v,x);
+A=W;
 for n=1:1:nt
                 Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
                 lambdax.*reshape(V2(2:nx-1,2:nv-1).*reshape(((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2),nx-2,nv-2),1,nx-2,nv-2)-...
                 epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
      n
+            %Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
+            %lambdax.*v(2:nv-1).*((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2)+...
+            %epsilonx.*(-Q(n,3:nx,2:nv-1)+2.*Q(n,2:nx-1,2:nv-1)-Q(n,1:nx-2,2:nv-1))/2;
+            %n
             
-            parfor i=1:1:nx
-                        for j=1:1:nv
-                                    W(i,j) = sum(sum(Theta(nx+i-(1:nx),j).*reshape(Q(n+1,1:nx,1:nv),nx,nv)));
-                        end
+            for i=1:1:nx
+                      % for j=1:1:nv
+                       %             W(i,j) = sum(sum(Theta(nx+i-(1:nx),j).*reshape(Q(n+1,1:nx,1:nv),nx,nv)));
+                       % end
+                                    
+                       W(i,1:nv) = sum(Thetaflip(nx-i+(1:nx),:)'*reshape(Q(n+1,1:nx,1:nv),nx,nv),2);
             end
             W = (x(2)-x(1)).*(v(2)-v(1)).*W;
              Q(n+1,2:nx-1,2:nv-1)=Q(n+1,2:nx-1,2:nv-1)-...
@@ -50,7 +63,10 @@ for n=1:1:nt
              epsilonv.*(-Q(n+1,2:nx-1,3:nv)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
             
          figure(1)
+         
          surf(X,V,reshape(Q(n,:,:),nx,nv));
+         shading interp
+         title(['time= ',num2str(t(n))])
          zlim([0,1]);
          colorbar;
          colormap(jet);
@@ -62,4 +78,3 @@ for n=1:1:nt
            % lambdav.*W(2:nx-1,2:nv-1).*(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2)) +...
            % epsilonv.*(-Q(n+1,2:nx-1,1:nv-2)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
 end
-
