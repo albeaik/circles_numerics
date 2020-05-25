5075b6b28ae34722b85e893906e8c3030bdfeeb7
saleh <albeaiksaleh@gmail.com>
2020-05-24 20:35:38 -0700

    split centroid and area computations

--- GetDelaunayAreas.m
+++ GetDelaunayAreas.m
@@ -1,11 +1,11 @@
-function [ centroids ] = GetDelaunayCentroids( DT )
-%GETDELAUNAYCENTROIDS Summary of this function goes here
+function [ areas ] = GetDelaunayAreas( DT )
+%GETDELAUNAYAREAS Summary of this function goes here
 %   Detailed explanation goes here
 
     trigs_x = reshape(DT.Points(DT.ConnectivityList', 1), [3, size(DT.ConnectivityList, 1)])';   %size = (num trigs, num vert per trig)
     trigs_y = reshape(DT.Points(DT.ConnectivityList', 2), [3, size(DT.ConnectivityList, 1)])';   %size = (num trigs, num vert per trig)
     
-    centroids = [mean(trigs_x, 2), mean(trigs_y, 2)];
+    areas = polyarea(trigs_x', trigs_y')';
 
 end
 
