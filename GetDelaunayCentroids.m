e5072207aa51b8ac0b6a6348ec4a87a8629bc3fa
saleh <albeaiksaleh@gmail.com>
2020-05-24 20:33:18 -0700

    victorize trig centroid and area calcs

--- GetDelaunayCentroids.m
+++ GetDelaunayCentroids.m
@@ -2,15 +2,11 @@ function [ centroids, areas ] = GetDelaunayCentroids( DT )
 %GETDELAUNAYCENTROIDS Summary of this function goes here
 %   Detailed explanation goes here
 
-    centroids = zeros([size(DT.ConnectivityList, 1), 2]);
-    areas = zeros([size(DT.ConnectivityList, 1), 1]);
-    parfor i = 1:size(DT.ConnectivityList, 1)
-        trig = DT.Points(DT.ConnectivityList(i, :), :);
-        trig_centroid = mean(trig);
-        centroids(i, :) = trig_centroid;
-        areas(i) = polyarea(trig(:, 1), trig(:, 2));
-    end
-
+    trigs_x = reshape(DT.Points(DT.ConnectivityList', 1), [3, size(DT.ConnectivityList, 1)])';   %size = (num trigs, num vert per trig)
+    trigs_y = reshape(DT.Points(DT.ConnectivityList', 2), [3, size(DT.ConnectivityList, 1)])';   %size = (num trigs, num vert per trig)
+    
+    centroids = [mean(trigs_x, 2), mean(trigs_y, 2)];
+    areas = polyarea(trigs_x', trigs_y')';
 
 end
 
