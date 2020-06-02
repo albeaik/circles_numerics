932f850ecf58340dcb24ae149b02fdb69963acf7
ahayat16 <61722512+ahayat16@users.noreply.github.com>
2020-06-02 20:40:26 +0200

    few adds

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -1,5 +1,7 @@
 clear all
 close all
+
+%Setting parameters
 nt=5e3;
 nx=2e2+1;
 nv=2e2;
@@ -11,9 +13,18 @@ x=linspace(-0,20,nx);
 v=linspace(0,20,nv);
 t=linspace(0,10,nt);
 
+
+%Initializing
+
 Q=zeros(nt+1,nx,nv);
 Q0=@(x,v) (x>=2).*(x<=5).*(v>=10).*(v<=15) + (x>=5).*(x<=15).*(v>=5).*(v<=10);
 
+y = zeros(nt+1);
+y0 = 0 ;
+w = zeros(nt+1);
+w0 = Vmax./3 ; 
+
+%Boundary conditions, useful functions
 
 lambdax=(t(2)-t(1))/(x(2)-x(1));
 lambdav=(t(2)-t(1))/(v(2)-v(1));
@@ -26,8 +37,18 @@ Q(1,:,end)=0;
 d0 = 2.5; % needed when define function Vf. Change later. 
 Vf=@(x) Vmax.*((tanh(x./d0-2)+tanh(2))./(1+tanh(2)));  
 h=@(x) exp(-(1)./((epsilon./2).^2-(-x-epsilon/2).^2)).*(x>-epsilon).*(x<0);
+
+%Kernels
+
 theta=@(x,v) alpha.*h(x).*(Vf(-x)-v).*(x>=-epsilon).*(x<=0);
 theta2=@(x,v) beta.*h(x).*(-v).*(x>=-epsilon).*(x<=0);
+
+
+theta3 = @(x,v,y)(alpha.*h(x-y).*(Vf(y-x)-v)); %Action of AV Bando
+theta4 = @(x,v,y,w)(beta.*h(x-y).*(w-v)); %Action of AV FTL
+
+%CFL and viscosity parameters
+
 epsilonx=1/3; %viscosity parameter, needs to be changed later to a proper value
 epsilonv=1/3;
 if lambdax > 2*min(3*epsilonx,2-3*epsilonx)/7
@@ -35,6 +56,9 @@ if lambdax > 2*min(3*epsilonx,2-3*epsilonx)/7
 elseif epsilonx>2/3 || epsilonx <0
     disp('Viscosity approximation out of range')
 end
+
+%Mesh and reshaping arrays
+
 x1 = linspace(-20,20,2.*nx);
 [V,X1] = meshgrid(v,x1);
 Theta = theta(X1,V);
@@ -81,6 +105,10 @@ W = zeros(nx,nv);
 [V2,~]=meshgrid(v,ones(1,nx));
 [V,X]=meshgrid(v,x);
 A=W;
+
+%Time iteration
+
+
 for n=1:1:nt
                 Q(n+1,2:nx-1,2:nv-1)=Q(n,2:nx-1,2:nv-1)-...
                 lambdax.*reshape(V2(2:nx-1,2:nv-1).*reshape(((Q(n,2:nx-1,2:nv-1)+Q(n,3:nx,2:nv-1))/2-(Q(n,1:nx-2,2:nv-1)+Q(n,2:nx-1,2:nv-1))/2),nx-2,nv-2),1,nx-2,nv-2)-...
