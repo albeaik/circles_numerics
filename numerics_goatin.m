18d1071dace3041fa778d34fdd4a2f1417c46553
YILINGU10 <yiling.you6003@gmail.com>
2020-06-07 21:51:06 -0700

    Accommodate multiple AVs (one lane)

--- numerics_goatin.m
+++ numerics_goatin.m
@@ -19,12 +19,13 @@ t=linspace(0,10,nt);
 Q=zeros(nt+1,nx,nv);
 Q0=@(x,v) (x>=2).*(x<=5).*(v>=10).*(v<=15) + (x>=5).*(x<=15).*(v>=5).*(v<=10);
 
-y = zeros(nt+1,1);
-y0 = 7 ;
-y(1) = y0;
-w = zeros(nt+1,1);
-w0 = 9*Vmax/10 ; 
-w(1) = w0;
+M = 2; %number of AVs
+y = zeros(nt+1,M);
+y0 = [7;9];
+y(1,:) = y0;
+w = zeros(nt+1,M);
+w0 = [9*Vmax/10;9*Vmax/10]; 
+w(1,:) = w0;
 
 %Boundary conditions, useful functions
 
@@ -50,8 +51,8 @@ theta=@(x,v) alpha.*h(x).*(Vf(-x)-v).*(x>=-epsilon).*(x<=0);
 theta2=@(x,v) beta.*h(x).*(-v).*(x>=-epsilon).*(x<=0);
 
 
-theta3 = @(x,v,y)(alpha.*h(x-y).*(Vf(y-x)-v)); %Action of AV Bando
-theta4 = @(x,v,y,w)(beta.*h(x-y).*(w-v)); %Action of AV FTL
+theta3 = @(x,v,y)(alpha.*h(x-y).*(Vf(y-x)-v)./M); %Action of AV Bando
+theta4 = @(x,v,y,w)(beta.*h(x-y).*(w-v)./M); %Action of AV FTL
 
 %CFL and viscosity parameters
 
@@ -147,7 +148,13 @@ for n=1:1:nt
             
 %COMMENT NEXT LINE AND UNCOMMENT PREVIOUS ONE IF DON'T WANT H2 - NOT CHECKED, HANDLE CAREFULLY
 %W(1:nx,1:nv) = sum(einsum(Thetab(1:nx,1:nv,1:nx),reshape(Q(n+1,1:nx,1:nv),nx,nv),3,1),3) + sum(einsum(Theta2b,reshape(Q(n+1,1:nx,1:nv),nx,nv),[3 4],[1 2]))+ theta3(X,V,y(n)) + theta4(X,V,y(n),w(n));
-W(1:nx,1:nv) = sum(einsum(Thetab(1:nx,1:nv,1:nx),reshape(Q(n+1,1:nx,1:nv),nx,nv),3,1),3) - (beta * Thetavb * (Thetaxb * reshape(Q(n+1,1:nx,1:nv),nx,nv))')' + theta3(X,V,y(n)) + theta4(X,V,y(n),w(n));
+ThetaW3M = zeros(nx, nv);
+ThetaW4M = zeros(nx, nv);
+for k = 1:M
+    ThetaW3M =  ThetaW3M + theta3(X,V,y(n,k));
+    ThetaW4M =  ThetaW4M + theta4(X,V,y(n,k),w(n,k));
+end
+W(1:nx,1:nv) = sum(einsum(Thetab(1:nx,1:nv,1:nx),reshape(Q(n+1,1:nx,1:nv),nx,nv),3,1),3) - (beta * Thetavb * (Thetaxb * reshape(Q(n+1,1:nx,1:nv),nx,nv))')' + ThetaW3M + ThetaW4M;
 %sum(dot(Thetaflip2(nx-:+(1:nx),nv-:+(1:nv)),reshape(Q(n+1,1:nx,1:nv),nx,nv)))
 %            end
             W = (x(2)-x(1)).*(v(2)-v(1)).*W;
@@ -155,8 +162,10 @@ W(1:nx,1:nv) = sum(einsum(Thetab(1:nx,1:nv,1:nx),reshape(Q(n+1,1:nx,1:nv),nx,nv)
              lambdav/2.*reshape(W(2:nx-1,2:nv-1).*(reshape(Q(n+1,2:nx-1,3:nv)-Q(n+1,2:nx-1,1:nv-2),nx-2,nv-2)),1,nx-2,nv-2) -...
              epsilonv.*(-Q(n+1,2:nx-1,3:nv)+2*Q(n+1,2:nx-1,2:nv-1)-Q(n+1,2:nx-1,1:nv-2))/2;
             %AV ODE update
-            y(n+1) = y(n) + w(n) * dt;
-            w(n+1) = w(n) + W(sum((y(n)-x)>=0),sum((w(n)-v)>=0)) * dt;
+            for k = 1:M
+                y(n+1,k) = y(n,k) + w(n,k) * dt;
+                w(n+1,k) = w(n,k) + W(sum((y(n,k)-x)>=0),sum((w(n,k)-v)>=0)) * dt;
+            end
             %y(n+1) = round(y(n+1)./dx).*dx;
             %w(n+1) = round(w(n+1)./dv).*dv;
 
@@ -171,7 +180,9 @@ W(1:nx,1:nv) = sum(einsum(Thetab(1:nx,1:nv,1:nx),reshape(Q(n+1,1:nx,1:nv),nx,nv)
          caxis([0 1]);
          view(0,90);
          hold on
-         plot(y(n+1),w(n+1),'r*')
+         for k = 1:M
+            plot(y(n+1,k),w(n+1,k),'r*')
+         end
          hold off
 
          drawnow;
