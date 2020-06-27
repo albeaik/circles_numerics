de860e466d01639e3c3fe8e1456eacbf2eca3c30
saleh <albeaiksaleh@gmail.com>
2020-06-26 17:38:45 -0700

    remove mesh edges from visualization

--- visualize_trig_trig.m
+++ visualize_trig_trig.m
@@ -3,11 +3,13 @@ function visualize_trig_trig( DT_plot, density_plot )
 %   Detailed explanation goes here
 
     %ref: https://www.mathworks.com/matlabcentral/answers/165624-how-to-color-trisurf-faces
+    
+    trianglation_edge_alpha = 0;
 
     hh = trisurf(DT_plot.ConnectivityList, DT_plot.Points(:, 1), DT_plot.Points(:, 2), DT_plot.Points(:, 2)*0+1);
     set(gca,'CLim',[min(density_plot), max(density_plot)]);
     set(hh,'FaceColor','flat', 'FaceVertexCData',density_plot, 'CDataMapping','scaled');
-    set(hh,'edgecolor', ([1 1 1])*0.3, 'EdgeAlpha', 0.3)
+    set(hh,'edgecolor', ([1 1 1])*0.3, 'EdgeAlpha', trianglation_edge_alpha)
     colorbar
     
     view(0,90)
