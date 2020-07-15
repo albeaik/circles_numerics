7b5f41dcf727b548d93dca31b34cbbd4b7f7f6c3
saleh <albeaiksaleh@gmail.com>
2020-07-14 18:45:08 -0700

    fix minor bug

--- pre_specified_initial_conditions.m
+++ pre_specified_initial_conditions.m
@@ -10,13 +10,13 @@ Q0_6 = @(x,v) (x >= 8) .* (x <= 12);
 
 
 
-[Q0_7.dbp1, Q0_7.dbe1] = get_rectangler_discontinuity_boundary( [0,12], [10,13], [0.1,0.5] ); 
-[Q0_7.dbp2, Q0_7.dbe2] = get_rectangler_discontinuity_boundary( [10,5], [20,7], [0.1,0.5] );
+[Q0_7.dbp1, Q0_7.dbe1] = get_rectangler_discontinuity_boundary( [0,12], [10,13], [0.5,1] ); 
+[Q0_7.dbp2, Q0_7.dbe2] = get_rectangler_discontinuity_boundary( [10,5], [20,7], [0.5,1] );
 Q0_7.oracle = @(x,v) (x>=0).*(x<=10).*(v>=12).*(v<=13) + (x>=10).*(x<=20).*(v>=5).*(v<=7);
 Q0_7.discontinuity_boundary_points = [Q0_7.dbp1; Q0_7.dbp2];
 Q0_7.discontinuity_boundary_edges = [Q0_7.dbe1; Q0_7.dbe2+size(Q0_7.dbp1, 1)];
 
-[Q0_8.dbp1, Q0_8.dbe1] = get_rectangler_discontinuity_boundary( [0,12], [10,13], [0.1,0.5] ); 
+[Q0_8.dbp1, Q0_8.dbe1] = get_rectangler_discontinuity_boundary( [0,12], [10,13], [0.5,1] ); 
 Q0_8.oracle = @(x,v) (x>=0).*(x<=10).*(v>=12).*(v<=13);
-Q0_8.discontinuity_boundary_points = [Q0_7.dbp1];
-Q0_8.discontinuity_boundary_edges = [Q0_7.dbe1];
+Q0_8.discontinuity_boundary_points = [Q0_8.dbp1];
+Q0_8.discontinuity_boundary_edges = [Q0_8.dbe1];
